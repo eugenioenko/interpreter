2f1c59c1d8b9f502569321a077e284687611eadb
eugenioenko <eugenioenko@gmail.com>
2019-02-08 22:36:28 -0500

    restored demo code

--- dist/bundle.js
+++ dist/bundle.js
@@ -153,7 +153,56 @@ class Console {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DemoSourceCode", function() { return DemoSourceCode; });
-const DemoSourceCode = ``;
+const DemoSourceCode = `// Recursive function
+function factorialize(n) {
+    if (n < 0) {
+        return -1;
+    }
+    if (n == 0) {
+        return 1;
+    }
+    return (n * factorialize(n - 1));
+}
+print factorialize(5);
+// Prototype objects
+function MyClass(text) {
+    this.text = text;
+}
+MyClass.method = function (text) {
+    this.text = this.text + text;
+};
+MyClass.count = function (times) {
+    function nested(num) {
+        return num * num;
+    }
+    for (let i = 0; i < times; ++i) {
+        print nested(i);
+    }
+};
+var instance = new MyClass('Hello ');
+instance.method('World');
+instance.count(3);
+print instance.text;
+// string length operator '$'
+print "the length of hellow world is: " + $"hello world";
+print  $"hello" === 6 ? 'hello is 5 character length' : 'it is not';
+// while loop and ternary operator
+let i = 0;
+while (i <= 4) {
+    print i % 2 ? 'odd' : 'even';
+    i = i + 1;
+}
+// literals
+var literal = {
+    firstname: "John",
+    lastname: "Doe",
+    records: {
+        prev: "previous",
+        next: "next"
+    }
+};
+print literal.records.prev;
+`;
 
 
 /***/ }),
--- src/demo.ts
+++ src/demo.ts
@@ -1,2 +1,51 @@
 export const DemoSourceCode =
-``;
+`// Recursive function
+function factorialize(n) {
+    if (n < 0) {
+        return -1;
+    }
+    if (n == 0) {
+        return 1;
+    }
+    return (n * factorialize(n - 1));
+}
+print factorialize(5);
+// Prototype objects
+function MyClass(text) {
+    this.text = text;
+}
+MyClass.method = function (text) {
+    this.text = this.text + text;
+};
+MyClass.count = function (times) {
+    function nested(num) {
+        return num * num;
+    }
+    for (let i = 0; i < times; ++i) {
+        print nested(i);
+    }
+};
+var instance = new MyClass('Hello ');
+instance.method('World');
+instance.count(3);
+print instance.text;
+// string length operator '$'
+print "the length of hellow world is: " + $"hello world";
+print  $"hello" === 6 ? 'hello is 5 character length' : 'it is not';
+// while loop and ternary operator
+let i = 0;
+while (i <= 4) {
+    print i % 2 ? 'odd' : 'even';
+    i = i + 1;
+}
+// literals
+var literal = {
+    firstname: "John",
+    lastname: "Doe",
+    records: {
+        prev: "previous",
+        next: "next"
+    }
+};
+print literal.records.prev;
+`;
