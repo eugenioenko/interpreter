10ab356e07624bb4b712cae9e31b6b7f309e4d15
eugenioenko <eugenioenko@gmail.com>
2019-03-08 08:43:42 -0500

    fixed merge conflicts and missing file

---
+++ src/regex.ts
@@ -0,0 +1,27 @@
+import { PrototypeEntity } from './entity';
+import * as Runtime from './runtime';
+import { Interpreter } from './interpreter';
+
+export class RegExEntity extends PrototypeEntity {
+    private regex: RegExp;
+
+    constructor(regex: RegExp) {
+        super();
+        this.regex = regex;
+        this.prototype.values.set('exec', Runtime.regexExecMethod(this));
+        this.prototype.values.set('test', Runtime.regexTestMethod(this));
+    }
+
+    public arity(): number {
+        return 0;
+    }
+
+    public call(interpreter: Interpreter, thiz: any, args: any[]): any {
+        return this;
+     }
+
+    public toString(): string {
+        return this.regex.toString();
+    }
+
+}
