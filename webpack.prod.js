925644e9fc1839ad4aa0941e31ea2812c52a2d31
eugenioenko <eyakhnenko@underline.com>
2023-06-30 11:34:36 -0700

    Fixed yarn build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,32 +1,29 @@
-const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: './src/atscript.ts',
-    devtool: 'source-map',
-    mode: 'production',
+    entry: "./src/atscript.ts",
+    devtool: "source-map",
+    mode: "production",
     watch: false,
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'
-        ]
+        extensions: [".tsx", ".ts", ".js"],
     },
     output: {
-        filename: 'atscript.min.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: "atscript.min.js",
+        path: path.resolve(__dirname, "dist"),
     },
     optimization: {
-        namedModules: false,
-        namedChunks: false,
-        nodeEnv: 'production',
-        minimizer: [new TerserPlugin()]
-    }
+        nodeEnv: "production",
+        minimizer: [new TerserPlugin()],
+    },
 };
